69091a07d9328aafb4db9f4d749560c9ab1f5308
black0596 <black0596@sina.com>
2017-05-16 17:08:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<img src="./assets/logo.png" />
+		<h1>{{msg}}</h1>
+		<firstcomponent></firstcomponent>
+	</div>
+</template>
+<script>
+import firstcomponent from './components/firstcomponent.vue'
+export default {
+	data() {
+		return {
+			msg: 'Hello Vue'
+		}
+	},
+	components:{firstcomponent}
+}
+</script>
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
---
+++ src/components/firstcomponent.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="firstcomponent">
+		<h1>i am a title.</h1>
+		<a> written by {{author}}</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				author:'baijiachaung'
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
